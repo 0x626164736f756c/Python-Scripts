fd1abdfaabaf47f22dbe1e068a65bb656549598c
medhee <gbaldous@free.fr>
2021-11-14 17:30:46 +0100

    add script

--- decodingPython/decodingAscii.py
+++ decodingPython/decodingAscii.py
@@ -5,7 +5,6 @@ def decoding():
     try:
         tab = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
         print(''.join(chr(i)for i in tab))
-
     except Exception as e:
         print(e)
 
---
+++ decodingPython/decodingByteEncoded.py
@@ -0,0 +1,21 @@
+#usr/bin
+
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import long_to_bytes
+
+#script for decoded (base16/10 or whatever) => long to byte for reading concern
+
+def decodingBase():
+    try:
+        str =  11515195063862318899931685488813747395775516287289682636499965282714637259206269
+        bytes = long_to_bytes(str)
+        print(bytes)
+    except Exception as e:
+        print(e)
+
+
+def main():
+    decodingBase()
+
+if __name__ == '__main__':
+    main()
