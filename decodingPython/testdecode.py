738b64a03ac1233dbeff4b8915df1cec8a1670bb
medhee <gbaldous@free.fr>
2021-11-16 22:54:56 +0100

    add identification of encoded string

--- decodingPython/testdecode.py
+++ decodingPython/testdecode.py
@@ -1,28 +1,94 @@
-#usr/bin
+#!/usr/bin/env python3
 
-sbase64 ="aGVsbG8gd29ybGQK"
-srot13 = "wrna iny wrna pelcgbpyvpx"
-shex = "666C61673A7B6765746C656C656C657D"
-sutf8 = "[97, 99, 113, 117, 105, 114, 101, 95, 115, 101, 101, 110, 95, 99, 97, 114, 105, 110, 10]"
-sbigint ="0x74686f6d736f6e5f6469766572736974795f68617070696e657373"
+import codecs
+import re
+import string
 
-def decoding(param1 : str):
+#sutf8 = "[97, 99, 113, 117, 105, 114, 101, 95, 115, 101, 101, 110, 95, 99, 97, 114, 105, 110, 10]" #no idea
+#srot13 = "wrna iny wrna pelcgbpyvpx" #No idea
+#sbase64 ="aGVsbG8gd29ybGQK" #alphanumerique
+#shex = "666C61673A7B6765746C656C656C657D" #alphanumerique
+#sbigint ="0x74686f6d736f6e5f6469766572736974795f68617070696e657373" #alphanumerique
+
+tabstr = ["[97, 99, 113, 117, 105, 114, 101, 95, 115, 101, 101, 110, 95, 99, 97, 114, 105, 110, 10]","wrna iny wrna pelcgbpyvpx","aGVsbG8gd29ybGQK","666C61673A7B6765746C656C656C657D","0x74686f6d736f6e5f6469766572736974795f68617070696e657373"]
+tabstr2 = ["wrna iny wrna pelcgbpyvpx","0x74686f6d736f6e5f6469766572736974795f68617070696e657373","[97, 99, 113, 117, 105, 114, 101, 95, 115, 101, 101, 110, 95, 99, 97, 114, 105, 110, 10]","aGVsbG8gd29ybGQK","666C61673A7B6765746C656C656C657D"]
+def decoding(encoded : str, methods : str):
     try:
-        #test encodage ut8-ascii
-        if ',' in param1:
+        if methods == "sutf8":
             try:
-                modifInput = param1[1:-1].replace(" ", "").split(',')
+                modifInput = encoded[1:-1].replace(" ","").split(',')
                 transInt = []
                 for c in modifInput:transInt.append(int(c))
                 final = ''.join(map(chr,transInt))
                 return str(final)
             except:
                 print("erreur 1")
+        #decoding rot13
+        elif methods == "srot13":
+            try:
+                pass
+            except:
+                print("erreur 2")
+        elif methods == "shex":
+            try:
+                pass
+            except :
+                pass
+        elif methods =="sbase64":
+            try:
+                pass
+            except:
+                pass
+        elif methods == "sbigint":
+            try:
+                pass
+            except :
+                pass      
     except:
         print("une erreur est apparue")
+
+
+def preparation(prepa : str):
+    temp =  prepa.replace(" ","")
+    isrot13 = checkrot13(prepa)
+    ishex = checkhex(prepa)
+    isbase64 = checkbase64(prepa)
+    if ","in temp:
+        return "sutf8"
+    elif isrot13==False:
+        return "srot13"
+    elif ishex==True:
+        return "shex"
+    elif isbase64==True:
+        return "sbase64"
+    else :
+        return "sbigint"
+
+
+def checkrot13(prepa :str):
+    x = re.findall("\d",prepa)
+    if x:
+        return True
+    else:
+        return False
+
+def checkhex(prepa :str):
+    x = all(c in string.hexdigits for c in prepa)
+    return x
+
+def checkbase64(prepa :str):
+    regexp = re.search('[ghijklmnopqrstuvwyzGHIJKLMNOPQRSTUVWYZ]',prepa)
+    if regexp:
+       return True
+    else: 
+        return False
+
+
 def main():
-    decoding(sutf8)
-
-
+    for c in tabstr2:
+        temp = preparation(c)
+        print(temp)
+    #decoding(encoded,temp)
+   
 if __name__ == '__main__':
         main()
