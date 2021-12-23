4512f931a67778826ae2213f65b3146004b71e2b
medhee <gbaldous@free.fr>
2021-12-23 15:14:44 +0100

    refactor all files name

--- 1-Decoding/Decoding_JWTs.py
+++ 1-Decoding/Decoding_JWTs.py
@@ -4,9 +4,12 @@ import jwt
 def main():
 
     token_string = "<your_input_token_here>"
+
     #access the header without the key
     temp_variable = jwt.get_unverified_header(token_string)
     print(temp_variable)
+
+    
     #read the all jwt without the key()
     temp_variable2 = jwt.decode(token_string, options={"verify_signature": False})
     print(temp_variable2)
