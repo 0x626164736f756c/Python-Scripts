3d60db80b800168dc1ad99a75a32eec61608c5c4
medhee <gbaldous@free.fr>
2022-01-10 20:48:40 +0100

    ADD reading RSA key

---
+++ 4-SOLVE/Solve_Read_RSA_Key.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# cf  : https://bitsdeep.com/posts/attacking-rsa-for-fun-and-ctf-points-part-1/
+#
+
+
+from Crypto.PublicKey import RSA
+
+
+def read_RSA_KEY():
+    with open("<your_key_.pem","r") as f:
+        key = RSA.import_key(f.read())
+        print(key.<the part that interest yourself -> example key.d for the private key>)
+      
+def main():
+    read_RSA_KEY()
+
+if __name__ == '__main__':
+    main()
